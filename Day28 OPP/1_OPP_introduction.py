350ff99c59c23ca4f6c85c3ce8130be8a1175882
shubham mangnale <shubhammangnale93@gmail.com>
2026-08-03 14:42:24 +0530

    Added Day28 OPP

---
+++ Day28 OPP/1_OPP_introduction.py
@@ -0,0 +1,73 @@
+'''
+Todays Agenda:
+===>> oop
+===>> class
+===>> object
+===>> ref variable
+===>> __init__method
+# OOP (Object-Oriented Programming) :- 
+OOP (Object-Oriented Programming) is a programming paradigm that organizes code using classes and objects.
+It helps make programs modular, reusable, secure, and easy to maintain.
+
+# What is a Class:-
+A class is a blueprint or template used to create objects. 
+It defines the attributes (data) and methods (functions) that an object will have.
+# Class = Blueprint (Template)
+
+# What is an Object:-
+An object is an instance of a class. 
+It contains actual data and can access the methods defined in the class.
+# Object = Real Thing (Instance)
+'''
+
+# class Cls_name:
+#     #block|code
+#     #prop
+#     #operation
+
+# Cls_name()
+# Cls_name()
+# Cls_name()
+# Cls_name()
+#---------------------------------------------------------------------------------
+
+# class Cls_name:
+#     #block|code
+#     #prop
+#     #operation
+# obj1 = Cls_name()
+# obj2 = Cls_name()    
+#----------------------------------------------------------------------------------
+
+# class Student():
+#     pass
+
+# class Employee():
+#     pass
+
+# raj = Student()
+# snehal = Student()
+# pratik = Employee()
+
+# print(type(Student()))
+# print(type(Employee()))
+# print(type(raj))
+# print(type(snehal))
+# print(type(pratik))
+#----------------------------------------------------------------------------------
+
+class Student():
+    pass
+
+class Employee():
+    pass
+
+s1 = Student()
+e1 = Employee()
+
+print(dir(Employee))
+print(dir(Student))
+#----------------------------------------------------------------------------------
+
+
+
